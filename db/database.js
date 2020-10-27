6f0ca812ee1e1bbe19ff6c42015412d2151a64b0
CRIMSON <olueatoworosanwo@email.com>
2020-10-27 01:57:51 +0100

    db pool

--- db/database.js
+++ db/database.js
@@ -2,47 +2,21 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
-function connectDb() {
-    const conn = mysql.createConnection({
-        host: "eu-cdbr-west-03.cleardb.net",
-        port: 3306,
-        user: "bf49b062b62ffb",
-        password: "c295ab38",
-        database: "heroku_7b86f29a392f3d2",
-        database_url:
-            "mysql://bf49b062b62ffb:c295ab38@eu-cdbr-west-03.cleardb.net/heroku_7b86f29a392f3d2?reconnect=true",
-    });
+const conn = mysql.createPool({
+    host: "eu-cdbr-west-03.cleardb.net",
+    port: 3306,
+    user: "bf49b062b62ffb",
+    password: "c295ab38",
+    database: "heroku_7b86f29a392f3d2",
+    database_url:
+        "mysql://bf49b062b62ffb:c295ab38@eu-cdbr-west-03.cleardb.net/heroku_7b86f29a392f3d2?reconnect=true",
+});
 
-    conn.connect(function (err) {
-        if (err) {
-            console.log("error when connecting to db:", err);
-            setTimeout(connectDb, 2000);
-        } else {
-            console.log("connected to DB");
-        }
-    });
-
-    conn.on("error", function (err) {
-        console.log("db error", err);
-        if (err.code === "PROTOCOL_CONNECTION_LOST") {
-            connectDb();
-        } else {
-            throw err;
-        }
-    });
-
-    return conn;
-}
-
-var conn = connectDb();
+conn.query("select 1 + 1", (err, rows) => {
+    /* */
+});
 
 let instance = null;
-
-const query = "SELECT * FROM data";
-conn.query(query, (err, results) => {
-    if (err) throw err;
-    console.log(results);
-});
 
 class dbservice {
     static getDbServiceInstance() {
