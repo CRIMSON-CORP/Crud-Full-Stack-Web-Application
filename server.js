fa5675790e9da82b396790e315f3e7873cd46223
CRIMSON <olueatoworosanwo@email.com>
2020-10-28 17:13:51 +0100

    fixed host in server

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-const express = require('express');
-const path = require('path');
-const router = require('./routes/routes')
-const cors = require('cors')
+const express = require("express");
+const path = require("path");
+const router = require("./routes/routes");
+const cors = require("cors");
 
 // Express Init
 const server = express();
 const PORT = process.env.PORT || 2000;
 
 // Express MIddle-ware
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, "public")));
 server.use(router);
 server.use(express.json());
-server.use(express.urlencoded({
-    extended: false
-}))
+server.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
 server.use(cors());
 
 // server Init
-server.listen(PORT, 'localhost', (err) => {
+server.listen(PORT, "0.0.0.0", (err) => {
     if (err) throw err;
     console.log(`Server running on port ${PORT}`);
-})
+});
